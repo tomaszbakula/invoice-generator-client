673ac00f281da36a754716612b0f1d817af0b173
Tomasz Bakula <tbakula92@gmail.com>
2017-05-07 22:42:02 +0100

    Front page: add hero & social stats.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,45 @@
 <template>
-  <section class="hero is-medium is-warning is-bold">
-    <div class="hero-body">
-      <div class="container">
-        <h1 class="title">
-          Generate Invoices
-        </h1>
-        <h2 class="subtitle">
-          as never before
-        </h2>
+  <div>
+
+    <section class="hero is-medium is-warning is-bold">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            Generate Invoices
+          </h1>
+          <h2 class="subtitle">
+            as never before
+          </h2>
+        </div>
       </div>
-    </div>
-  </section>
+    </section>
+
+    <nav class="section content level is-mobile">
+      <div class="level-item has-text-centered">
+        <div>
+          <p class="heading">Tweets</p>
+          <p class="title">3,456</p>
+        </div>
+      </div>
+      <div class="level-item has-text-centered">
+        <div>
+          <p class="heading">Following</p>
+          <p class="title">123</p>
+        </div>
+      </div>
+      <div class="level-item has-text-centered">
+        <div>
+          <p class="heading">Followers</p>
+          <p class="title">456K</p>
+        </div>
+      </div>
+      <div class="level-item has-text-centered">
+        <div>
+          <p class="heading">Likes</p>
+          <p class="title">789</p>
+        </div>
+      </div>
+    </nav>
+
+  </div>
 </template>
